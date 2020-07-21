8ca5844d822ec957b2a89fc71c3d4a775ffa4331
grassswwang <androidhuoniao@hotmail.com>
2020-07-21 10:38:58 +0800

    添加bigTextStyle Notification测试用例

--- app/src/main/java/com/grass/helloandroid/fragment/NotificationFragment.kt
+++ app/src/main/java/com/grass/helloandroid/fragment/NotificationFragment.kt
@@ -1,6 +1,19 @@
 package com.grass.helloandroid.fragment
 
+import android.R
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.airbnb.epoxy.EpoxyController
+import com.grass.helloandroid.SecondActivity
 import com.grass.helloandroid.views.marquee
 import com.grass.helloandroid.views.previewItemView
 
@@ -12,18 +25,19 @@ import com.grass.helloandroid.views.previewItemView
  * Email: grassswwang@tencent.com
  */
 class NotificationFragment : BaseEpoxyFragment() {
+    var CALENDAR_ID: String? = "channel_01"
 
     override fun EpoxyController.buildModels() {
         marquee {
-            id("keep live")
-            title("保活")
+            id("notificatioo")
+            title("通知样式")
         }
 
         previewItemView {
-            id("start_alarm")
-            title("start alarm")
+            id("bigtextstyle")
+            title("BigTextStyle")
             clickListener { _ ->
-
+                showNotification(activity!!)
             }
         }
 
@@ -40,5 +54,67 @@ class NotificationFragment : BaseEpoxyFragment() {
             clickListener { _ ->
             }
         }
+    }
+
+    private fun showNotification(context: Context) {
+        var notificationManager = NotificationManagerCompat.from(activity!!)
+        val notification: Notification
+        val builder: NotificationCompat.Builder
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationManager.createNotificationChannel(makeNotificationChannel())
+        }
+        builder = NotificationCompat.Builder(context, CALENDAR_ID!!)
+        val pendingIntent = PendingIntent.getActivity(
+            context, 11,
+            Intent(context, SecondActivity::class.java), 0
+        )
+        notification = builder.setSmallIcon(R.mipmap.sym_def_app_icon)
+            .setContentTitle("My notification")
+            .setContentText("Much longer text that cannot fit one line...")
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText(
+                        "无论是否意识到 Gradle 的存在，每位 Android 程序员都会直接或间接的与 Gradle 打交道。每当通过 Android Studio " +
+                                "新建一个工程时，AS 都会自动创建一个通用的目录结构，然后就可以进行开发，" +
+                                "在 app 的 build.gradle 中添加一些依赖，点击右上角的 Sync Now"
+                    )
+            )
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .build()
+        notificationManager.notify(System.currentTimeMillis().toInt(), notification)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun makeNotificationChannel(): NotificationChannel {
+        val notificationChannel = NotificationChannel(
+            CALENDAR_ID, "123",
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        // 既可以使用apply，也可以使用with
+        notificationChannel.apply {
+            // 设置渠道描述
+            description = "测试通知组"
+            // 是否绕过请勿打扰模式
+            canBypassDnd()
+            // 设置绕过请勿打扰模式
+            setBypassDnd(true)
+            // 桌面Launcher的消息角标
+            canShowBadge()
+            // 设置显示桌面Launcher的消息角标
+            setShowBadge(true)
+            // 设置通知出现时声音，默认通知是有声音的
+            setSound(null, null)
+            // 设置通知出现时的闪灯（如果 android 设备支持的话）
+            enableLights(true)
+            lightColor = Color.RED
+            // 设置通知出现时的震动（如果 android 设备支持的话）
+            enableVibration(true)
+            vibrationPattern = longArrayOf(
+                100, 200, 300, 400, 500, 400,
+                300, 200, 400
+            )
+        }
+        return notificationChannel;
     }
 }
