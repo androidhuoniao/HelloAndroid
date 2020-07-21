814a1768e0dde7ab9389f8993671499d2576912f
grassswwang <androidhuoniao@hotmail.com>
2020-07-21 11:39:24 +0800

    bigTextStyle Notification测试用例,添加右侧的icon显示

--- app/src/main/java/com/grass/helloandroid/fragment/NotificationFragment.kt
+++ app/src/main/java/com/grass/helloandroid/fragment/NotificationFragment.kt
@@ -7,6 +7,7 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Build
 import androidx.annotation.RequiresApi
@@ -71,8 +72,16 @@ class NotificationFragment : BaseEpoxyFragment() {
         notification = builder.setSmallIcon(R.mipmap.sym_def_app_icon)
             .setContentTitle("My notification")
             .setContentText("Much longer text that cannot fit one line...")
+                // 显示通知右侧的icon
+            .setLargeIcon(
+            BitmapFactory.decodeResource(
+                context.getResources(),
+                R.mipmap.sym_def_app_icon
+            )
+        )
             .setStyle(
                 NotificationCompat.BigTextStyle()
+                    .setBigContentTitle("i am bigTextStyle notification")
                     .bigText(
                         "无论是否意识到 Gradle 的存在，每位 Android 程序员都会直接或间接的与 Gradle 打交道。每当通过 Android Studio " +
                                 "新建一个工程时，AS 都会自动创建一个通用的目录结构，然后就可以进行开发，" +
