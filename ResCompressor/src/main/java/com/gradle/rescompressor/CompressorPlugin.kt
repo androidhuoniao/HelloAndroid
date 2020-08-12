0c9bd6481043aa490ea7c2852084c77d0bcaf00d
grass <grassswwang@tencent.com>
2020-08-12 22:12:20 +0800

    重写apply函数

--- ResCompressor/src/main/java/com/gradle/rescompressor/CompressorPlugin.kt
+++ ResCompressor/src/main/java/com/gradle/rescompressor/CompressorPlugin.kt
@@ -1,5 +1,9 @@
 package com.gradle.rescompressor
 
+import com.android.build.gradle.AppExtension
+import com.android.build.gradle.LibraryExtension
+import com.gradle.rescompressor.gradle.getAndroid
+import com.gradle.rescompressor.task.compression.cwebp.CwebpCompressionVariantProcessor
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -9,9 +13,29 @@ import org.gradle.api.Project
  * on 2020/8/12
  * Email: grassswwang@tencent.com
  */
-class CompressorPlugin :Plugin<Project>{
+class CompressorPlugin : Plugin<Project> {
 
-    override fun apply(proj: Project) {
+    override fun apply(project: Project) {
         println("${this.javaClass.simpleName}.apply() is working")
+        when {
+            project.plugins.hasPlugin("com.android.application") -> project.getAndroid<AppExtension>()
+                .let { android ->
+                    project.afterEvaluate {
+                        var processor = CwebpCompressionVariantProcessor()
+                        android.applicationVariants.forEach { variant ->
+                            processor.process(variant)
+                        }
+                    }
+                }
+            project.plugins.hasPlugin("com.android.library") -> project.getAndroid<LibraryExtension>()
+                .let { android ->
+                    project.afterEvaluate {
+                        var processor = CwebpCompressionVariantProcessor()
+                        android.libraryVariants.forEach { variant ->
+                            processor.process(variant)
+                        }
+                    }
+                }
+        }
     }
 }
