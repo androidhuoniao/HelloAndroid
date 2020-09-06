16d1d78c488767d4b016893de7adf4613c82c966
grass <grassswwang@tencent.com>
2020-09-06 19:02:27 +0800

    添加一个测试类

---
+++ resourses/src/main/java/com/grass/helloandroid/resources/HelloResources.java
@@ -0,0 +1,12 @@
+package com.grass.helloandroid.resources;
+
+/**
+ * Created by grassswwang
+ * on 2020/9/6
+ * Email: grassswwang@tencent.com
+ */
+public class HelloResources {
+    public void test() {
+
+    }
+}
